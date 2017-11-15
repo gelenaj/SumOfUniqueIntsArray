c67c324a9c9f131efffa42ef2c352871a2ee4e6b
Gloria Jimenez <gelenaj@gmail.com>
2017-11-15 16:19:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace uniqueIntArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myArr = new List<int>() { 1, 2, 2, 3,3 };
+            
+            Console.WriteLine("Given int array {1, 2, 2, 3, 3 }, the sum of unique integers is:"+ addUnique(myArr));
+            Console.ReadLine();
+        }
+
+        public static int addUnique(List<int> myArr)
+        {
+            int sum = 0;
+            for(int i = 0; i < myArr.Count();i++)
+            {
+                if(myArr.Count(x=>x == myArr[i]) == 1)
+                {
+                    sum += myArr[i];
+                }
+            }
+                
+            return sum;
+        }
+    }
+}
